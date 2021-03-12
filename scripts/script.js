3003ff6dc9984472e716814cab10706961c062da
alternateved <alternateved@gmail.com>
2021-03-12 13:43:28 +0100

    Add playerFactory function

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,3 @@
-const gameBoard = ((doc) => {
-  const board = Array.from(doc.querySelectorAll(".game-container > div"));
-  return board;
-})(document);
-
-const displayController = (() => {
-
-})();
-
-const playerFactory = () => {
-
-;}
-
-console.log(gameBoard);
+const playerFactory = (name, marker) => {
+  return {name, marker};
+};
