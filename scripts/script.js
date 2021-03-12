04fbefea3d5682c2498232a1925bf4e134923006
alternateved <alternateved@gmail.com>
2021-03-12 13:49:42 +0100

    Create gameBoard module

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,28 @@
 const playerFactory = (name, marker) => {
   return {name, marker};
 };
+const gameBoard = (() => {
+  const board = Array.from(document.querySelectorAll(".game-container > div"));
+  const array = [
+    [],[],[],
+    [],[],[],
+    [],[],[]
+  ];
+  const clear = () => board.forEach(square => square.textContent = "");
+  const render = () => {
+    board.forEach((node) =>
+      node.addEventListener("click", (event) => {
+        if (event.target.textContent) {
+        } else {
+          event.target.classList.add("x");
+          event.target.textContent = "X";
+        }
+      })
+    );
+  };
+  return { clear, render };
+})();
+
+const displayController = (() => {})();
+
+gameBoard.render();
