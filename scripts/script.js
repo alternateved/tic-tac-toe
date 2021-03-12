db2a54f83974325100b6e0c054069081320708ca
alternateved <alternateved@gmail.com>
2021-03-12 12:45:20 +0100

    Add responsivity to the page

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const gameBoard = ((doc) => {
+  const board = Array.from(doc.querySelectorAll(".game-container > div"));
+  return board;
+})(document);
+
+const displayController = (() => {
+
+})();
+
+const playerFactory = () => {
+
+;}
+
+console.log(gameBoard);
